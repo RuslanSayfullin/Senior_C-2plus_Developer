ef2e640d8c179332db262d153423c5bb91ef9cd4
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-10-02 10:30:11 +0500

    Функция set_symmetric_difference используется для нахождения симметрической разницы между двумя упорядоченными множествами (set) и записи её в другое множество. Симметрическая разница включает в себя элементы, которые присутствуют в одном из множеств, но не в обоих.

---
+++ set_symmetric_difference.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+int main() {
+    std::set<int> set1 = {1, 2, 3, 4};
+    std::set<int> set2 = {3, 4, 5, 6};
+    std::set<int> symmetric_difference;
+
+    std::set_symmetric_difference(set1.begin(), set1.end(), set2.begin(), set2.end(), std::inserter(symmetric_difference, symmetric_difference.begin()));
+
+    std::cout << "Симмертическая разница: ";
+    for (const auto& element : symmetric_difference) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
