18f220c5116c264b6959da5af052536ccdc8097c
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-10-26 10:06:33 +0500

    std::memory_order_acquire — это один из флагов (memory order) в стандартной библиотеке C++, который используется в контексте многопоточности и атомарных операций.

---
+++ memory_order_acquire.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <atomic>
+#include <thread>
+
+std::atomic<int> data(0);
+
+void producer() {
+	data.store(42, std::memory_order_release);
+}
+
+void consumer() {
+	int value;
+	while (!(value = data.load(std::memory_order_acquire))) {
+		std::this_thread::yield();
+	}
+	std::cout << "Received: " << value << std::endl;
+}
+
+int main() {
+	std::thread producerThread(producer);
+	std::thread consumerThread(consumer);
+
+	producerThread.join();
+	consumerThread.join();
+
+	return 0;
+}
