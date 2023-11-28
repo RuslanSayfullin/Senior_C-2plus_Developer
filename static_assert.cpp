214af908c4fff49d2ba0866700048a653d0ea305
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-11-28 09:39:23 +0500

    static_assert — это механизм в C++, который позволяет выполнять проверки на этапе компиляции для статических условий. Он был добавлен в стандарт C++11 и предоставляет способ проверки, что определенное условие истинно во время компиляции. Если условие ложно, компиляция завершится ошибкой.

---
+++ static_assert.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+template <typename T>
+void CheckSize() {
+	static_assert(sizeof(T) >= 4, "Type size must be at least 4 bytes.");
+}
+
+int main() {
+	CheckSize<int>();
+
+	return 0;
+}
