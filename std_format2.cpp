f794a715a4cb99fe02e7fd8de3ffdd3e1444e818
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-01-23 18:50:29 +0500

    std::format
    std::format — это функция в C++, которая используется для форматирования текста. Она может использоваться для вставки значений переменных, строк и других объектов в шаблон текста.

---
+++ std_format2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <format>
+
+int main() {
+	int age = 30;
+	std::string name = "John Doe";
+
+	std::cout << std::format("My name is {1} and I am {0} years old.\n", age, name);
+	return 0;
+}
