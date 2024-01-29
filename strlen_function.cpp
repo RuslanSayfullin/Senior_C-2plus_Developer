5ac4caae0a78f68af73926a64f485f9174b90044
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-01-29 09:59:20 +0500

    Функция strlen() в языке программирования C++ используется для определения длины строки. Она принимает в качестве аргумента указатель на строку и возвращает значение типа size_t, которое представляет собой количество символов в строке, включая нулевой символ \0, который завершает строку.

---
+++ strlen_function.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstring>
+
+int main() {
+	char str[] = "Hello, world";
+	size_t len = strlen(str);
+
+	std::cout << "Длина строки: " << len << std::endl;
+
+	return 0;
+}
