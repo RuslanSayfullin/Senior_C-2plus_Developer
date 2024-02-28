78b3865b45755e36875473bdfb93aab13fe06d7c
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-02-28 12:09:59 +0500

    <unistd.h> — это заголовочный файл в языке программирования C и C++, который предоставляет доступ к некоторым системным вызовам (system calls) в операционных системах Unix-подобных, таких как Linux.

--- floor.cpp
+++ floor.cpp
@@ -5,7 +5,7 @@ int main() {
 	double number = 3.7;
 	double result = floor(number);
 
-	std::cout << "исходное число: " << number << std::endl;
+	std::cout << "Исходное число: " << number << std::endl;
 	std::cout << "Округленное вниз число: " << result << std::endl;
 
 	return 0;
