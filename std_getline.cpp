13a4772cf024ef7290781b3d777bf2a7f3cf0b21
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-04-18 14:19:01 +0500

    Функция std::getline в C++ используется для чтения строк из потока ввода, обычно с клавиатуры. Она является частью стандартной библиотеки C++ (iostream) и позволяет считывать строки, содержащие пробелы, в отличие от оператора извлечения (>>), который останавливается на первом пробеле.

---
+++ std_getline.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string name;
+	std::cout << "Ваедите ваше имя: ";
+	std::getline(std::cin, name);
+	std::cout << "Привет, " << name << "!" << std::endl;
+	return 0;
+}
