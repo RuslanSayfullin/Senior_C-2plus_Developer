2dcdf84f6276199c81ad09c26fa1209ba1e717cb
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-03-27 10:52:36 +0500

    Функция reverse_copy относится к библиотеке <algorithm> и используется для копирования элементов из одного диапазона в другой в обратном порядке.

---
+++ std_reverse_copy.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main() {
+    std::vector<int> source = {1, 2, 3, 4, 5};
+    std::vector<int> target(5); // Создаем целевой вектор с достаточнм размером
+
+    std::reverse_copy(source.begin(), source.end(), target.begin());
+
+    for (const int& value : target) {
+        std::cout << value << " ";
+    }
+    
+    return 0;
+}
