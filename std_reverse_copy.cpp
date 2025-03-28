e9e46a1a0648c322c77eb5aa9e5a5041b85d77b4
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-03-28 09:51:45 +0500

    std::launch::async указывает на то, что функция должна быть немедленно запущена в отдельном потоке (асинхронно).

--- std_reverse_copy.cpp
+++ std_reverse_copy.cpp
@@ -2,6 +2,9 @@
 #include <algorithm>
 #include <vector>
 
+/*
+Функция reverse_copy относится к библиотеке <algorithm> и используется для копирования элементов из одного диапазона в другой в обратном порядке. 
+*/
 int main() {
     std::vector<int> source = {1, 2, 3, 4, 5};
     std::vector<int> target(5); // Создаем целевой вектор с достаточнм размером
