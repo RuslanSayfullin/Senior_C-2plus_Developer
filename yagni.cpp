bdb0b32333968bf132d9540a4d0c77936aa5a4d5
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-04-07 11:58:55 +0500

    Принцип YAGNI (You Ain't Gonna Need It)

---
+++ yagni.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class Point {
+public:
+	Point(double x, double y) : x_(x), y_(y) {}
+
+	double getX() const { return x_; }
+	double getY() const { return y_; }
+
+private:
+	double x_;
+	double y_;
+};
+
+int main() {
+	Point p1(1.0, 2.0);
+	Point p2(4.0, 6.0);
+
+	return 0;
+}
