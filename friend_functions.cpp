8a58c5b02cb22d4d756ddd8c4ca874a81d153d11
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-04-07 11:50:01 +0500

    функции, которые объявлены как дружественные внутри класса и имеют доступ к закрытым и защищенным членам этого класса

---
+++ friend_functions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class MyClass {
+private:
+	int x;
+public:
+	MyClass(int val) : x(val) {}
+	friend void showValue(const MyClass& obj);
+};
+
+// Дружественная функция, имеющая доступ к закрытому члену класса
+void showValue(const MyClass& obj) {
+	std::cout << "Значение x: " << obj.x << std::endl;
+}
+
+int main() {
+	MyClass obj(42);
+	showValue(obj);	// Можем вызвать дружественную функцию
+	return 0;
+}
