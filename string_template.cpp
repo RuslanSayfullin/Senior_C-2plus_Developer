59ef1dd61e3dcab1ab261960e6d9093dfd5dbdce
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-04-14 09:41:18 +0500

    Строковые литералы как параметры шаблона

---
+++ string_template.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+template<std::basic_fixed_string T>
+
+class Foo {
+    static constexpr char const* Name = T;
+public:
+    void hello() const; 
+};
+
+int main() {
+    Foo<"Hello!"> foo;
+    foo.hello();
+}
