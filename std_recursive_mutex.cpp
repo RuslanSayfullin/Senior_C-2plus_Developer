c2c79d40aadaf4bbe345632cd218bcc6b6a3bce2
Sayfullin Ruslan <ruslansaifullin91@gmail.com>
2025-06-23 15:42:37 +0500

    Рекурсивный мьютекс (recursive mutex) — это специальный тип мьютекса, который позволяет одному и тому же потоку многократно захватывать мьютекс, не приводя к блокировке.

---
+++ std_recursive_mutex.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+std::recursive_mutex mtx;  // Объявляем recursive_mutex в глобальной области видимости
+
+void foo(int x) {
+    mtx.lock();
+    std::cout << "Locked by thread" << x << std::endl;
+
+    // Вложенный вызов мьютекса не вызывает блокировку
+    mtx.lock();
+    std::cout << "Nested lock by thread: " << x << std::endl;
+
+    mtx.unlock();
+    std::cout << "Unlocked by thread: " << x << std::endl;
+
+    mtx.unlock();
+}
+
+int main(){
+    std::thread t1(foo, 1);
+    std::thread t2(foo, 2);
+
+    t1.join();
+    t2.join();
+}
