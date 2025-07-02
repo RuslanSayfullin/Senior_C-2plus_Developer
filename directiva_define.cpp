7f1101373dde1a24e11ccad3a5769813ea2cee26
Sayfullin Ruslan <ruslansaifullin91@gmail.com>
2025-07-02 09:32:27 +0500

    Директива define используется для создания макросов, которые позволяют вам задавать символьные константы или небольшие фрагменты кода, которые будут заменены компилятором на определенное значение или код перед компиляцией программы.

---
+++ directiva_define.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#define PI 3.14159
+
+int main() {
+    double radius = 5.8;
+    double area = PI * radius * radius;
+
+    std::cout << "Circle area: " << area << std::endl;
+    
+    return 0;
+}
