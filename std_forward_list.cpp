a53e31d68e62b8ce590253c78478c73f1113c92a
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-10-15 01:32:22 -0400

    Forward_list в C++ - это контейнер, поддерживающий быструю вставку и удаление элементов из любого места в контейнере. Быстрый доступ к случайным элементам не поддерживается. Реализуется как односвязный список. По сравнению с std::list этот контейнер обеспечивает более эффективное хранение памяти, когда двунаправленная итерация не требуется.

---
+++ std_forward_list.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <forward_list>
+
+int main() {
+    std::forward_list<int> list = {1, 2, 3, 4, 5};
+
+    // Добавление элемента в начало контейнера
+    list.push_front(0);
+
+    // Добавление элемента в конец контейнера
+    //list.push_back(6);
+
+    // Удаление первого элемента контейнера
+    list.pop_front();
+
+    // Удаление последнего элемента контейнера
+    //list.pop_back();
+
+    // Итерация по контейнеру
+    for (std::forward_list<int>::iterator it = list.begin(); it != list.end(); ++it) {
+        std::cout << *it << " ";
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
