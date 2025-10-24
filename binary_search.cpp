5d5839d6fce3b1b4f2eefdf999abb6db501e0e42
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-10-24 00:40:56 -0400

    Бинарный поиск

---
+++ binary_search.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int bsearch(const vector<int> &arr, int l, int r, int q) {
+    while (l <= r) {
+        int mid = l + (r-l)/2;
+        if (arr[mid] == q) return mid;
+
+        if (q < arr[mid]) {
+                r = mid - 1;
+            }
+        else {
+            l = mid + 1;
+        }
+    }
+    return -1; // не нашли
+}
+
+
+int main() {
+    int query = 10;
+    int arr[] = {2, 4, 6, 8, 10, 10};
+    int N = sizeof(arr) / sizeof(arr[0]);
+
+    vector<int> v(arr, arr + N);
+
+    // Сортируем входной массив
+    sort(v.begin(), v.end());
+    int idx;
+    idx = bsearch(v, 0, v.size(), query);
+
+    if (idx != -1) 
+        cout << "бинарный поиск: нашли по индексу " << idx << endl;
+    else
+        cout << "бинарный поиск, не нашли" << endl;
+
+    return 0;
+
+}
