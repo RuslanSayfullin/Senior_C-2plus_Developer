5ef87a086e9b6483c229887fc428494dc20781fb
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-12-04 08:48:20 -0500

    std::unique используется для удаления дубликатов любого элемента, присутствующего последовательно в диапазоне [первый, последний).

---
+++ std_unique_copy.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    vector<int> v = {10, 10, 30, 30, 30, 100, 10, 300, 300, 70, 70, 80 };
+
+    // Declaring a vector to store the copied value
+    vector<int> v1(10);
+
+    vector<int>::iterator ip;
+
+    // Using std::unique_copy
+    ip = std::unique_copy(v.begin(), v.begin() + 12, v1.begin());
+
+    // Resizing vector v1
+    v1.resize(std::distance(v1.begin(), ip));
+
+    cout << "Before: ";
+    for (ip = v.begin(); ip != v.end(); ++ip)
+    {
+        cout << *ip << " ";
+    }
+
+    cout << "\nAfter: ";
+    for (ip = v1.begin(); ip != v1.end(); ++ip)
+    {
+        cout << *ip << " ";
+    }
+
+    return 0;
+}
