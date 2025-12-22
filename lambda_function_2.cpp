dc5714a988b7d98894e074a7e95ce97f3e93d376
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-12-22 09:51:48 +0500

    Лямбда-функция (или просто лямбда) — это анонимная функция в C++, которая может быть определена непосредственно внутри кода. Лямбда-функции предоставляют более компактный и удобный способ создания небольших функций на лету, без необходимости объявления их отдельно.

---
+++ lambda_function_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::vector<int> numbers = {1, 2, 3, 4, 5};
+
+    // используем лямбду для вывода каждого числа
+    std::for_each(numbers.begin(), numbers.end(), [](int num) {
+        std::cout << num << " ";
+    });
+    std::cout << std::endl;
+
+    return 0;
+}
