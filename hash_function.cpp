1a394b543104357ecde00a4bd8c5d736750314cd
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2025-12-26 11:28:57 +0500

    Хеш-функция - это функция, которая принимает на вход некоторые данные и возвращает фиксированный размер выходных данных, называемый хешем.

---
+++ hash_function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <functional>
+
+int main() {
+    std::string str = "Hello, world!";
+
+    std::hash<std::string> str_hash;    // создание обьекта хеш-функции для строки
+    std::size_t hash_val = str_hash(str);   // вычисление хеша для строки
+
+    std::cout << "Хэш-значение для строки: " << str << " равно " << hash_val << std::endl;
+
+    return 0;
+}
