1225c555aa9cdd6d03ed3fc9df9cb6860102a478
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2026-01-08 10:22:41 +0500

    Алгоритм accumulate
    Возвращает сумму всех значений, лежащих в диапазоне между [first, last) с переменной init.

---
+++ accumulate.cpp
@@ -0,0 +1,24 @@
+#include <iostream>     // std::cout
+#include <functional>   // std::minus
+#include <numeric>      // std::accumulate
+
+int myfunction (int x, int y) {return x+2*y;}
+
+int main() {
+    int init = 100;
+    int numbers[] = {10, 20, 30};
+
+    std::cout << "using default accumulate: ";
+    std::cout << std::accumulate(numbers, numbers+3, init);
+    std::cout << '\n';
+
+    std::cout << "using functional's minus: ";
+    std::cout << std::accumulate(numbers, numbers+3, init, std::minus<int>());
+    std::cout << '\n';
+
+    std::cout << "using custom function: ";
+    std::cout << std::accumulate(numbers, numbers+3, init, myfunction);
+    std::cout << '\n';
+
+    return 0;
+}
