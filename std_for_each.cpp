96c392eec757d7f938f4b931111dc98eaa04666b
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2026-01-30 09:57:38 +0500

    Функция принимает список в качестве входных данных и применяет пользовательскую функцию к каждому элементу этого списка. Это полезно, когда нам нужно выполнить одну и ту же операцию со всеми элементами списка.

---
+++ std_for_each.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+void doubleNumber(int &i) {
+    i *= 2;
+}
+
+int main() {
+    std::vector<int> arr = { 1, 2, 3, 4 };
+
+    std::for_each(arr.begin(), arr.end(), doubleNumber);
+
+    for (int i : arr) {
+        std::cout << i << ' ';
+    }
+
+    std::cout << '\n';
+
+    return 0;
+}
