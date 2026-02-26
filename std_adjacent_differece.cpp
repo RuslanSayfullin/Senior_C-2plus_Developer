169ce4da9830b0001f3644e4dec657c4c984b035
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2026-02-26 12:20:41 +0500

    Функция std::adjacent_difference() из библиотеки стандартных алгоритмов C++ вычисляет последовательные различия между каждым элементом и его предшественником в входном диапазоне. Результаты выводятся в диапазон назначения.

---
+++ std_adjacent_differece.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+
+int main() {
+    int arr[] = {1, 2, 3, 5, 11, 12};
+    int result[7];
+
+    std::adjacent_difference(arr, arr + 7, result);
+
+    for (int i = 0; i < 7; i++) {
+        std::cout << result[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
