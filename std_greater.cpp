3cbeef7ecf973f2cc9c58bc94a748bcf8b37ad11
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2026-02-27 19:05:27 +0500

    Шаблон std::greater представляет собой функциональный объект, который используется для сравнения двух объектов по возрастанию.

---
+++ std_greater.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    int numbers[] = { 1, 3, 2, 5, 4 };
+
+    std::sort(numbers, numbers + 5, std::greater<int>());
+
+    for (int i = 0; i + 5; i++) {
+        std::cout << numbers[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
